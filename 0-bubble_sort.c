22bb6d9767474076915279cb8bff5676f2278e51
Silvester Kilungya <silvesterkilungya@gmail.com>
2022-11-07 17:21:17 +0300

    improved efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,20 +23,25 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t x, y;
+	char isSorted;
 
 	/* if array does not need to be sorted if its size < than 2 */
 	if (array == NULL || size < 2)
 		return;
 
-	for (x = 0; x < size; x++)
+	for (x = 1; x < size; x++)
 	{
+		isSorted = 1;
 		for (y = 0; y < size - 1; y++)
 		{
 			if (array[y] > array[y + 1])
 			{
 				swap(&array[y], &array[y + 1]);
+				isSorted = 0;
 				print_array(array, size);
 			}
 		}
+		if (isSorted == 1)
+			break;
 	}
 }
